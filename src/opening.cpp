953397fba930b2c79df81b77dc8c84e1dc5a8c1d
othman.elbaz <othman.elbaz@epita.fr>
2022-10-11 11:07:37 +0000

    changed algo and convert vector ==> array

--- src/opening.cpp
+++ src/opening.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
 
-void print(std::vector<std::vector<unsigned char>> input)
+void print(unsigned char **input, size_t height, size_t width)
 {
-    for (size_t i = 0; i < input.size(); i++)
+    for (size_t i = 0; i < height; i++)
     {
         auto line = input[i];
-        for (size_t j = 0; j < line.size(); j++)
+        for (size_t j = 0; j < width; j++)
         {
-            std::cout << (int) line[j];
+            std::cout << (int)line[j];
         }
         std::cout << '\n';
     }
@@ -17,21 +17,32 @@ void print(std::vector<std::vector<unsigned char>> input)
 /*
 Verify if the kernel is included in the image at the position (i, j)
 */
-int intersection(std::vector<std::vector<unsigned char>> input, std::vector<std::vector<unsigned char>> kernel, int i, int j)
+int product(unsigned char **input, unsigned char **kernel, int i, int j,
+            size_t height, size_t width, size_t height_kernel,
+            size_t width_kernel, bool dilation)
 {
-    size_t height_kernel = kernel.size();
-    size_t width_kernel = kernel[0].size();
-    size_t height = input.size();
-    size_t width = input[0].size();
     int res = 0;
 
     if ((i + height_kernel) > height || (j + width_kernel) > width)
         return 0;
 
-    for (size_t k = 0; k < height_kernel; k ++)
-        for (size_t l = 0; l < width_kernel; l ++)
-            if (input[k + i][l + j] != 0 && kernel[k][l] != 0)
-                res += kernel[k][l];
+    int cst_height = (height - 1) / 2;
+    int cst_width = (width - 1) / 2;
+    for (size_t k = 0; k < height_kernel; k++)
+    {
+        for (size_t l = 0; l < width_kernel; l++)
+        {
+            auto mul =
+                input[k + i - cst_height][l + j - cst_width] * kernel[k][l];
+            if (mul > res && dilation)
+                res = mul;
+            if (!dilation && (res == 0 || mul < res))
+                res = mul;
+        }
+    }
+    /*
+        if (input[k + i][l + j] != 0 && kernel[k][l] != 0)
+            res += kernel[k][l];*/
 
     return res;
 }
@@ -40,37 +51,32 @@ int intersection(std::vector<std::vector<unsigned char>> input, std::vector<std:
 Fill the output image with the values of the input image at the positions where
 the kernel is equal to one
 */
-std::vector<std::vector<unsigned char>> fill_opening(std::vector<std::vector<unsigned char>> input, std::vector<std::vector<unsigned char>> output, std::vector<std::vector<unsigned char>> kernel, int i, int j)
+unsigned char **fill_opening(unsigned char **input, unsigned char **output,
+                             unsigned char **kernel, int i, int j,
+                             size_t height, size_t width, size_t height_kernel,
+                             size_t width_kernel)
 {
-    size_t height_kernel = kernel.size();
-    size_t width_kernel = kernel[0].size();
-    size_t height = input.size();
-    size_t width = input[0].size();
-
     if ((i + height_kernel) > height || (j + width_kernel) > width)
         return output;
 
-    for (size_t k = 0; k < height_kernel; k ++)
-        for (size_t l = 0; l < width_kernel; l ++)
+    for (size_t k = 0; k < height_kernel; k++)
+        for (size_t l = 0; l < width_kernel; l++)
             if (kernel[k][l] != 0)
                 output[k + i][l + j] = input[k + i][l + j] * kernel[k][l];
 
     return output;
 }
 
-
-std::vector<std::vector<unsigned char>> fill_closing(std::vector<std::vector<unsigned char>> input, std::vector<std::vector<unsigned char>> output, std::vector<std::vector<unsigned char>> kernel, int i, int j)
+unsigned char **fill_closing(unsigned char **input, unsigned char **output,
+                             unsigned char **kernel, int i, int j,
+                             size_t height, size_t width, size_t height_kernel,
+                             size_t width_kernel)
 {
-    size_t height_kernel = kernel.size();
-    size_t width_kernel = kernel[0].size();
-    size_t height = input.size();
-    size_t width = input[0].size();
-
     if ((i + height_kernel) > height || (j + width_kernel) > width)
         return output;
 
-    for (size_t k = 0; k < height_kernel; k ++)
-        for (size_t l = 0; l < width_kernel; l ++)
+    for (size_t k = 0; k < height_kernel; k++)
+        for (size_t l = 0; l < width_kernel; l++)
             if (kernel[k][l] != 0)
                 output[k + i][l + j] = 0;
 
@@ -80,29 +86,31 @@ std::vector<std::vector<unsigned char>> fill_closing(std::vector<std::vector<uns
 /*
 Creates a 2D vector filled with the value
 */
-template  < typename  T >
-std::vector<std::vector<T>> create_vector2D(size_t height, size_t width, T value)
+template <typename T>
+T **create_array2D(size_t height, size_t width, T value)
 {
-    std::vector<std::vector<T>> res;
+    T **res = (T **)malloc(height * sizeof(T *));
     for (size_t i = 0; i < height; i++)
     {
-        std::vector<unsigned char> line(width, value);
-        res.emplace_back(line);
+        T *line = (T *)malloc(width * sizeof(T));
+        for (size_t j = 0; j < width; j++)
+        {
+            line[j] = value;
+        }
+        res[i] = line;
     }
     return res;
 }
 
 /*
-Sum the elements of a create_vector2D
+Sum the elements of a create_array2D
 */
-template  < typename  T >
-int sum_vector(std::vector<std::vector<T>> vect)
+template <typename T>
+int sum_vector(T **vect, size_t height, size_t width)
 {
     int res = 0;
-    size_t height = vect.size();
     if (height == 0)
         return 0;
-    size_t width = vect[0].size();
     for (size_t i = 0; i < height; i++)
         for (size_t j = 0; j < width; j++)
             res += vect[i][j];
@@ -112,20 +120,18 @@ int sum_vector(std::vector<std::vector<T>> vect)
 /*
 DeepCopy of a vector2D
 */
-template  < typename  T >
-std::vector<std::vector<T>> deep_copy(std::vector<std::vector<T>> vect)
+template <typename T>
+T **deep_copy(T **vect, size_t height, size_t width)
 {
-    size_t height = vect.size();
-    size_t width = vect[0].size();
-    std::vector<std::vector<T>> res;
+    T **res = (T **)malloc(height * sizeof(T *));
     for (size_t i = 0; i < height; i++)
     {
-        std::vector<T> line;
+        T *line = (T *)malloc(width * sizeof(T));
         for (size_t j = 0; j < width; j++)
         {
-            line.push_back(vect[i][j]);
+            line[j] = vect[i][j];
         }
-        res.emplace_back(line);
+        res[i] = line;
     }
     return res;
 }
@@ -133,12 +139,10 @@ std::vector<std::vector<T>> deep_copy(std::vector<std::vector<T>> vect)
 /*
 DeepCopy of a vector2D
 */
-template  < typename  T >
-std::vector<std::vector<T>> add_padding(std::vector<std::vector<T>> vect, int pad_h, int pad_w)
+template <typename T>
+T **add_padding(T **vect, size_t height, size_t width, int pad_h, int pad_w)
 {
-    size_t height = vect.size();
-    size_t width = vect[0].size();
-    std::vector<std::vector<T>> res = create_vector2D<T>(height + 2 * pad_h, width + 2 * pad_w, 0);
+    T **res = create_array2D<T>(height + 2 * pad_h, width + 2 * pad_w, 0);
     for (size_t i = 0; i < height; i++)
         for (size_t j = 0; j < width; j++)
             res[i + pad_h][j + pad_w] = vect[i][j];
@@ -149,20 +153,51 @@ std::vector<std::vector<T>> add_padding(std::vector<std::vector<T>> vect, int pa
 /*
 Compute the morphological opening of the image
 */
-std::vector<std::vector<unsigned char>> perform_opening(std::vector<std::vector<unsigned char>> input, std::vector<std::vector<unsigned char>> kernel)
+unsigned char **perform_dilation(unsigned char **input, unsigned char **kernel,
+                                 size_t height, size_t width,
+                                 size_t height_kernel, size_t width_kernel)
 {
-    size_t height = input.size();
-    size_t width = input[0].size();
-    int kernel_sum = sum_vector(kernel);
-    auto output = create_vector2D<unsigned char>(height, width, 0);
-    for (size_t i = 0; i < height; i++)
+    int kernel_sum = sum_vector(kernel, height_kernel, width_kernel);
+    auto output = create_array2D<unsigned char>(height, width, 0);
+    for (size_t i = height_kernel; i < height - height_kernel; i++)
     {
-        for (size_t j = 0; j < width; j++)
+        for (size_t j = width_kernel; j < width - width_kernel; j++)
         {
-            if (intersection(input, kernel, i, j) == kernel_sum)
+            output[i][j] = product(input, kernel, i, j, height, width,
+                                   height_kernel, width_kernel, true);
+            /*
+            if (intersection(input, kernel, i, j, height, width, height_kernel,
+                             width_kernel)
+                == kernel_sum)
             {
-                output = fill_opening(input, output, kernel, i, j);
-            }
+                output = fill_opening(input, output, kernel, i, j, height,
+                                      width, height_kernel, width_kernel);
+            }*/
+        }
+    }
+    return output;
+}
+
+unsigned char **perform_erosion(unsigned char **input, unsigned char **kernel,
+                                size_t height, size_t width,
+                                size_t height_kernel, size_t width_kernel)
+{
+    int kernel_sum = sum_vector(kernel, height_kernel, width_kernel);
+    auto output = create_array2D<unsigned char>(height, width, 0);
+    for (size_t i = height_kernel; i < height - height_kernel; i++)
+    {
+        for (size_t j = width_kernel; j < width - width_kernel; j++)
+        {
+            output[i][j] = product(input, kernel, i, j, height, width,
+                                   height_kernel, width_kernel, false);
+            /*
+            if (intersection(input, kernel, i, j, height, width, height_kernel,
+                             width_kernel)
+                == kernel_sum)
+            {
+                output = fill_opening(input, output, kernel, i, j, height,
+                                      width, height_kernel, width_kernel);
+            }*/
         }
     }
     return output;
@@ -171,40 +206,41 @@ std::vector<std::vector<unsigned char>> perform_opening(std::vector<std::vector<
 /*
 Compute the average
 */
-int calculate_average(std::vector<std::vector<unsigned char>> mat, std::vector<std::vector<unsigned char>> kernel, int i, int j)
+int calculate_average(unsigned char **mat, unsigned char **kernel, int i, int j,
+                      size_t height_kernel, size_t width_kernel)
 {
-    size_t height_kernel = kernel.size();
-    size_t width_kernel = kernel[0].size();
     int sum = 0;
     int quantity = 0;
-    int left = - (width_kernel / 2);
-    int top = - (height_kernel / 2);
-    for (size_t k = 0; k < height_kernel; k++) {
-        for (size_t l = 0; l < width_kernel; l++) {
+    int left = -(width_kernel / 2);
+    int top = -(height_kernel / 2);
+    for (size_t k = 0; k < height_kernel; k++)
+    {
+        for (size_t l = 0; l < width_kernel; l++)
+        {
             if (kernel[k][l] != 0 && mat[i + k + left][j + l + top] != 0)
             {
                 quantity += 1;
                 sum += mat[i + k + left][j + l + top];
             }
-
         }
     }
     return sum / quantity;
 }
 
-std::vector<std::vector<unsigned char>> mask_closing(std::vector<std::vector<unsigned char>> output, std::vector<std::vector<unsigned char>> mask, std::vector<std::vector<unsigned char>> kernel)
+unsigned char **mask_closing(unsigned char **output, unsigned char **mask,
+                             unsigned char **kernel, size_t height,
+                             size_t width, size_t height_kernel,
+                             size_t width_kernel)
 {
-    size_t height = output.size();
-    size_t width = output[0].size();
-    size_t height_pad = kernel.size();
-    size_t width_pad = kernel[0].size();
-    for (size_t i = 0; i < (height); i++)
+    for (size_t i = 0; i < height; i++)
     {
-        for (size_t j = 0; j < (width); j++)
+        for (size_t j = 0; j < width; j++)
         {
-            if (mask[i + height_pad][j + width_pad] != 0 && output[i][j] == 0)
+            if (mask[i + height_kernel][j + width_kernel] != 0
+                && output[i][j] == 0)
             {
-                output[i][j] = calculate_average(output, kernel, i, j);
+                output[i][j] = calculate_average(output, kernel, i, j,
+                                                 height_kernel, width_kernel);
             }
         }
     }
@@ -214,50 +250,84 @@ std::vector<std::vector<unsigned char>> mask_closing(std::vector<std::vector<uns
 /*
 Compute the morphological closing of the image
 */
-std::vector<std::vector<unsigned char>> perform_closing(std::vector<std::vector<unsigned char>> input, std::vector<std::vector<unsigned char>> kernel)
+/*
+unsigned char **perform_closing(unsigned char **input, unsigned char **kernel,
+                                size_t height, size_t width,
+                                size_t height_kernel, size_t width_kernel)
 {
-    size_t height = input.size();
-    size_t width = input[0].size();
-    size_t height_kernel = kernel.size();
-    size_t width_kernel = kernel[0].size();
-    auto input_pad = add_padding<unsigned char>(input, height_kernel, width_kernel);
-    auto mask = create_vector2D<unsigned char>(height + 2 * height_kernel, width + 2 * width_kernel, 1);
-    auto output = deep_copy<unsigned char>(input);
+    auto input_pad = add_padding<unsigned char>(input, height, width,
+                                                height_kernel, width_kernel);
+    auto mask = create_array2D<unsigned char>(height + 2 * height_kernel,
+                                              width + 2 * width_kernel, 1);
+    auto output = deep_copy<unsigned char>(input, height, width);
 
     for (size_t i = 0; i < (height + 2 * height_kernel); i++)
     {
         for (size_t j = 0; j < (width + 2 * width_kernel); j++)
         {
-            if (intersection(input_pad, kernel, i, j) == 0)
+            if (product(input_pad, kernel, i, j, height, width, height_kernel,
+                        width_kernel)
+                == 0)
             {
-                mask = fill_closing(input_pad, mask, kernel, i, j);
+                mask = fill_closing(input_pad, mask, kernel, i, j, height,
+                                    width, height_kernel, width_kernel);
             }
         }
     }
-    output = mask_closing(output, mask, kernel);
+    output = mask_closing(output, mask, kernel, height, width, height_kernel,
+                          width_kernel);
     return output;
 }
-
+*/
 int main()
 {
-    std::vector<std::vector<unsigned char>> input = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
-        {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 5, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 0, 0, 5},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    unsigned char **input = create_array2D<unsigned char>(11, 29, 0);
+    unsigned char input_t[11][29] = {
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+          0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+          0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+          0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+        { 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+          5, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 0, 0, 5 },
+        { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+          5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
     };
-    std::vector<std::vector<unsigned char>> k1 = {{1,1,1}, {1,1,1}, {1,1,1}};
-    std::vector<std::vector<unsigned char>> k2 = {{0,1,0}, {1,1,1}, {0,1,0}};
-    auto output = perform_closing(input, k1);
-    print(input);
+    for (size_t i = 0; i < 11; i++)
+    {
+        for (size_t j = 0; j < 29; j++)
+        {
+            input[i][j] = input_t[i][j];
+        }
+    }
+    unsigned char **k1 = create_array2D<unsigned char>(3, 3, 1);
+    unsigned char **k2 = create_array2D<unsigned char>(3, 3, 0);
+    unsigned char k2_t[3][3] = { { 0, 1, 0 }, { 1, 1, 1 }, { 0, 1, 0 } };
+    for (size_t i = 0; i < 3; i++)
+    {
+        for (size_t j = 0; j < 3; j++)
+        {
+            k2[i][j] = k2_t[i][j];
+        }
+    }
+    auto output = perform_erosion(input, k1, 11, 29, 3, 3);
+    output = perform_dilation(output, k1, 11, 29, 3, 3);
+    print(input, 11, 29);
     std::cout << '\n';
-    print(output);
+    print(output, 11, 29);
+
     return 0;
 }
