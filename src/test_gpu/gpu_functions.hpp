52e2908a2adc15f8424fa684e72ab0884d4a53b1
Moustapha Diop <moustephdiop@gmail.com>
2022-11-25 17:41:19 +0100

    (refacto) files

--- src/test_gpu/gpu_functions.hpp
+++ src/test_gpu/gpu_functions.hpp
@@ -3,4 +3,8 @@
 #include <memory>
 
 int test_function(unsigned char* buffer, size_t rows, size_t cols);
+
 unsigned char otsu_threshold(unsigned char* buffer, size_t rows, size_t cols, size_t pitch);
+
+int threshold(unsigned char *buffer, size_t rows, size_t cols, size_t pitch);
+
