3fdf62268c4eedbc32b19e0e0fd5c3b50779fb3a
Mathieu Rivier <mathieu.rivier@epita.fr>
2022-11-25 10:24:04 +0100

    refacto(threshold) - rename threshold and add comments)

--- src/bench.cpp
+++ src/bench.cpp
@@ -4,98 +4,115 @@
 #include <benchmark/benchmark.h>
 #include <iostream>
 
-
-void BM_gray_scale(benchmark::State& st) {
+void BM_gray_scale(benchmark::State& st)
+{
     int width, height, channels;
     std::string ref_image_path = "../images/base.png";
-    
-    unsigned char *ref_image = load_image(const_cast<char *>(ref_image_path.c_str()), &width, &height, &channels);
-    struct ImageMat *image = new_matrix(height, width);
+
+    unsigned char* ref_image = load_image(
+        const_cast<char*>(ref_image_path.c_str()), &width, &height, &channels);
+    struct ImageMat* image = new_matrix(height, width);
 
     for (auto _ : st) {
         to_gray_scale(ref_image, image, width, height, 3);
     }
-    
-    st.counters["frame_rate"] = benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
+
+    st.counters["frame_rate"] =
+        benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
 }
 
-void BM_blurring(benchmark::State& st) {
+void BM_blurring(benchmark::State& st)
+{
     int width, height, channels;
     std::string ref_image_path = "../images/base.png";
-    
-    load_image(const_cast<char *>(ref_image_path.c_str()), &width, &height, &channels);
-    struct ImageMat *image = new_matrix(height, width);
-    struct ImageMat *temp_image = new_matrix(height, width);
-    
+
+    load_image(const_cast<char*>(ref_image_path.c_str()), &width, &height,
+               &channels);
+    struct ImageMat* image = new_matrix(height, width);
+    struct ImageMat* temp_image = new_matrix(height, width);
+
     struct GaussianKernel* g_kernel = create_gaussian_kernel(5);
-    
+
     for (auto _ : st)
         apply_blurring(image, temp_image, g_kernel);
-    
-    st.counters["frame_rate"] = benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
+
+    st.counters["frame_rate"] =
+        benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
 }
 
-void BM_difference(benchmark::State& st) {
+void BM_difference(benchmark::State& st)
+{
     int width, height, channels;
     std::string ref_image_path = "../images/base.png";
-    
-    load_image(const_cast<char *>(ref_image_path.c_str()), &width, &height, &channels);
-    struct ImageMat *image1 = new_matrix(height, width);
-    struct ImageMat *image2 = new_matrix(height, width);
-    
+
+    load_image(const_cast<char*>(ref_image_path.c_str()), &width, &height,
+               &channels);
+    struct ImageMat* image1 = new_matrix(height, width);
+    struct ImageMat* image2 = new_matrix(height, width);
+
     for (auto _ : st)
         difference(image1, image2);
-    
-    st.counters["frame_rate"] = benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
+
+    st.counters["frame_rate"] =
+        benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
 }
 
-void BM_closing(benchmark::State& st) {
+void BM_closing(benchmark::State& st)
+{
     int width, height, channels;
     std::string ref_image_path = "../images/base.png";
-    
-    load_image(const_cast<char *>(ref_image_path.c_str()), &width, &height, &channels);
+
+    load_image(const_cast<char*>(ref_image_path.c_str()), &width, &height,
+               &channels);
     struct MorphologicalKernel* k1 = circular_kernel(5);
-    struct ImageMat *image1 = new_matrix(height, width);
-    struct ImageMat *image2 = new_matrix(height, width);
-    
+    struct ImageMat* image1 = new_matrix(height, width);
+    struct ImageMat* image2 = new_matrix(height, width);
+
     for (auto _ : st) {
         perform_erosion(image1, image2, k1);
         perform_dilation(image1, image2, k1);
     }
-    
-    st.counters["frame_rate"] = benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
+
+    st.counters["frame_rate"] =
+        benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
 }
 
-void BM_opening(benchmark::State& st) {
+void BM_opening(benchmark::State& st)
+{
     int width, height, channels;
     std::string ref_image_path = "../images/base.png";
-    
-    load_image(const_cast<char *>(ref_image_path.c_str()), &width, &height, &channels);
+
+    load_image(const_cast<char*>(ref_image_path.c_str()), &width, &height,
+               &channels);
     struct MorphologicalKernel* k1 = circular_kernel(11);
-    struct ImageMat *image1 = new_matrix(height, width);
-    struct ImageMat *image2 = new_matrix(height, width);
-    
+    struct ImageMat* image1 = new_matrix(height, width);
+    struct ImageMat* image2 = new_matrix(height, width);
+
     for (auto _ : st) {
         perform_dilation(image1, image2, k1);
         perform_erosion(image1, image2, k1);
     }
-    
-    st.counters["frame_rate"] = benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
+
+    st.counters["frame_rate"] =
+        benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
 }
 
-void BM_threshold(benchmark::State& st) {
+void BM_threshold(benchmark::State& st)
+{
     int width, height, channels;
     std::string ref_image_path = "../images/base.png";
-    
-    load_image(const_cast<char *>(ref_image_path.c_str()), &width, &height, &channels);
-    struct ImageMat *image1 = new_matrix(height, width);
-    struct ImageMat *image2 = new_matrix(height, width);
-    
+
+    load_image(const_cast<char*>(ref_image_path.c_str()), &width, &height,
+               &channels);
+    struct ImageMat* image1 = new_matrix(height, width);
+    struct ImageMat* image2 = new_matrix(height, width);
+
     for (auto _ : st) {
-        compute_threshold(image1, image2);
+        threshold(image1, image2);
     }
-    
-    st.counters["frame_rate"] = benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
+
+    st.counters["frame_rate"] =
+        benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
 }
 
 void BM_main_cpu(benchmark::State& st)
@@ -104,19 +121,23 @@ void BM_main_cpu(benchmark::State& st)
     std::string obj_image_path = "../images/obj.png";
 
     int width, height, channels;
-    unsigned char *ref_image = load_image(const_cast<char *>(ref_image_path.c_str()), &width, &height, &channels);
-    unsigned char *obj_image = load_image(const_cast<char *>(obj_image_path.c_str()), &width, &height, &channels);
-    
-    unsigned char **images = (unsigned char **) std::malloc(sizeof(unsigned char *) * 2);
+    unsigned char* ref_image = load_image(
+        const_cast<char*>(ref_image_path.c_str()), &width, &height, &channels);
+    unsigned char* obj_image = load_image(
+        const_cast<char*>(obj_image_path.c_str()), &width, &height, &channels);
+
+    unsigned char** images =
+        (unsigned char**)std::malloc(sizeof(unsigned char*) * 2);
     images[0] = ref_image;
     images[1] = obj_image;
 
-    int *nb_objs = (int *) std::malloc(1 * sizeof(int));
+    int* nb_objs = (int*)std::malloc(1 * sizeof(int));
 
     for (auto _ : st)
         main_detection(images, 2, width, height, channels, nb_objs);
 
-    st.counters["frame_rate"] = benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
+    st.counters["frame_rate"] =
+        benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
 }
 
 /* void BM_Rendering_gpu(benchmark::State& st) */
@@ -127,36 +148,23 @@ void BM_main_cpu(benchmark::State& st)
 /*   /1* for (auto _ : st) *1/ */
 /*   /1*   detect_gpu(data.data(), data.data(), width, height, stride); *1/ */
 
-/*   st.counters["frame_rate"] = benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate); */
+/*   st.counters["frame_rate"] = benchmark::Counter(st.iterations(),
+ * benchmark::Counter::kIsRate); */
 /* } */
 
-BENCHMARK(BM_gray_scale)
-->Unit(benchmark::kMillisecond)
-->UseRealTime();
+BENCHMARK(BM_gray_scale)->Unit(benchmark::kMillisecond)->UseRealTime();
 
-BENCHMARK(BM_blurring)
-->Unit(benchmark::kMillisecond)
-->UseRealTime();
+BENCHMARK(BM_blurring)->Unit(benchmark::kMillisecond)->UseRealTime();
 
-BENCHMARK(BM_difference)
-->Unit(benchmark::kMillisecond)
-->UseRealTime();
+BENCHMARK(BM_difference)->Unit(benchmark::kMillisecond)->UseRealTime();
 
-BENCHMARK(BM_closing)
-->Unit(benchmark::kMillisecond)
-->UseRealTime();
+BENCHMARK(BM_closing)->Unit(benchmark::kMillisecond)->UseRealTime();
 
-BENCHMARK(BM_opening)
-->Unit(benchmark::kMillisecond)
-->UseRealTime();
+BENCHMARK(BM_opening)->Unit(benchmark::kMillisecond)->UseRealTime();
 
-BENCHMARK(BM_threshold)
-->Unit(benchmark::kMillisecond)
-->UseRealTime();
+BENCHMARK(BM_threshold)->Unit(benchmark::kMillisecond)->UseRealTime();
 
-BENCHMARK(BM_main_cpu)
-->Unit(benchmark::kMillisecond)
-->UseRealTime();
+BENCHMARK(BM_main_cpu)->Unit(benchmark::kMillisecond)->UseRealTime();
 /* BENCHMARK(BM_Rendering_gpu) */
 /* ->Unit(benchmark::kMillisecond) */
 /* ->UseRealTime(); */
--- src/threshold/threshold_cpu.cpp
+++ src/threshold/threshold_cpu.cpp
@@ -19,29 +19,24 @@ void apply_bin_threshold(struct ImageMat* in_image,
                          unsigned char threshold)
 {
     for (int x = 0; x < in_image->height; ++x)
-        for (int y = 0; y < in_image->width; ++y) {
+        for (int y = 0; y < in_image->width; ++y)
             out_image->pixel[x][y] = 255 * (in_image->pixel[x][y] >= threshold);
-        }
 }
 
-void compute_otsu_threshold(struct ImageMat* in_image, struct ImageMat* temp)
+int threshold(struct ImageMat* base_image, struct ImageMat* temp_image)
 {
-    // TODO: Get two images for the connexe components
-    // TODO: But that's after we do a full initial cleanup!
-
-    unsigned char otsu_thresh = otsu_threshold(in_image);
+    // PART 1 - Otsu thresholds
+    unsigned char otsu_thresh = otsu_threshold(base_image);
     unsigned char otsu_thresh2 = otsu_thresh * 2.5;
 
     // First threshold saved to out_image_1
-    apply_base_threshold(in_image, otsu_thresh - 10);
+    apply_base_threshold(base_image, otsu_thresh - 10);
 
     // Second threshold saved to out_image_2
-    apply_bin_threshold(in_image, temp, otsu_thresh2);
-}
+    apply_bin_threshold(base_image, temp_image, otsu_thresh2);
 
-int compute_threshold(struct ImageMat* base_image, struct ImageMat* temp_image)
-{
-    compute_otsu_threshold(base_image, temp_image);
+    // PART 2 - Connexe Components
+    // Compute connexe components
     int nb_compo = connexe_components(base_image, temp_image);
 
     swap_matrix(base_image, temp_image);
