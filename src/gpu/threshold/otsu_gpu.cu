64d30d7a63cbb05c266a1d513bd663fd70cd97ff
Moustapha Diop <moustephdiop@gmail.com>
2022-11-28 11:45:40 +0100

    (add) opti otsu

--- src/gpu/threshold/otsu_gpu.cu
+++ src/gpu/threshold/otsu_gpu.cu
@@ -8,134 +8,130 @@
 float inf = std::numeric_limits<float>::infinity();
 
 // See slides to improve
-__global__ void sum_black_white(unsigned char *buffer, size_t rows, size_t cols, size_t pitch, int threshold,
-                                unsigned int *sum_white, unsigned int *sum_black) {
-    int col = blockDim.x * blockIdx.x + threadIdx.x;
-    int row = blockDim.y * blockIdx.y + threadIdx.y;
-
-    if (col >= cols || row >= rows)
-        return;
-
-    if (buffer[col + row * pitch] >= threshold) 
-        atomicAdd(sum_white, buffer[col + row * pitch]);
-    else
-        atomicAdd(sum_black, buffer[col + row * pitch]);
-    
-}
-
-__global__ void almost_var_black_white(unsigned char *buffer, size_t rows, size_t cols, size_t pitch,
-                                       int threshold, float *mean_white, float *mean_black,
-                                       float *sum_white, float *sum_black) {
-    int col = blockDim.x * blockIdx.x + threadIdx.x;
-    int row = blockDim.y * blockIdx.y + threadIdx.y;
-
-    if (col >= cols || row >= rows)
-        return;
-
-    if (buffer[col + row * pitch] >= threshold) {
-        float val = ((float) buffer[col + row * pitch] - *mean_white) * ((float) buffer[col + row * pitch] - *mean_white);
-        atomicAdd(sum_white, val);
-    }
-    else {
-        float val = ((float) buffer[col + row * pitch] - *mean_black) * ((float) buffer[col + row * pitch] - *mean_black);
-        atomicAdd(sum_black, val);
-    }
-}
-
-
-__global__ void nb_bw_pixels(unsigned char *buffer, size_t rows, size_t cols, size_t pitch,
-                          unsigned int *sum_white, unsigned int *sum_black, int threshold) {
+__global__ void otsu(unsigned char *buffer, size_t rows, size_t cols, size_t pitch, int threshold,
+                     unsigned int *nb_white, unsigned int *nb_black,
+                     unsigned int *sum_white, unsigned int *sum_black,
+                     unsigned int *sum2_white, unsigned int *sum2_black) {
     int col = blockDim.x * blockIdx.x + threadIdx.x;
     int row = blockDim.y * blockIdx.y + threadIdx.y;
 
     if (col >= cols || row >= rows) {
         return;
     }
+    
+    __shared__ unsigned int local_nb_white;
+    __shared__ unsigned int local_nb_black;
+    
+    __shared__ unsigned int local_sum_white;
+    __shared__ unsigned int local_sum_black;
+    
+    __shared__ unsigned int local_sum2_white;
+    __shared__ unsigned int local_sum2_black;
 
-    if (buffer[col + row * pitch] >= threshold)
-        atomicAdd(sum_white, 1);
-    else
-        atomicAdd(sum_black, 1);
+    if (threadIdx.x == 0 && threadIdx.y == 0) {
+        local_nb_white = 0;
+        local_nb_black = 0;
+        local_sum_black = 0;
+        local_sum_white = 0;
+        local_sum2_black = 0;
+        local_sum2_white = 0;
+    }
+
+    __syncthreads();
+
+    unsigned char val = buffer[col + row * pitch];
+
+    if (val >= threshold) {
+        atomicAdd(&local_nb_white, 1);
+        atomicAdd(&local_sum_white, val);
+        atomicAdd(&local_sum2_white, val * val);
+    }
+    else {
+        atomicAdd(&local_nb_black, 1);
+        atomicAdd(&local_sum_black, val);
+        atomicAdd(&local_sum2_black, val * val);
+    }
+    
+    __syncthreads();
+    
+    if (threadIdx.x == 0 && threadIdx.y == 0) {
+        atomicAdd(nb_white, local_nb_white);
+        atomicAdd(nb_black, local_nb_black);
+        atomicAdd(sum_white, local_sum_white);
+        atomicAdd(sum_black, local_sum_black);
+        atomicAdd(sum2_black, local_sum2_black);
+        atomicAdd(sum2_white, local_sum2_white);
+    }
 }
 
-__global__ void mean(float* dst, unsigned int* sum, unsigned int *size) {
-    *dst = (float) *sum / (float) *size;
+__global__ void final_work(unsigned int *nb_white, unsigned int *nb_black,
+                      unsigned int *sum_white, unsigned int *sum_black,
+                      unsigned int *sum2_white, unsigned int *sum2_black,
+                      float *result) {
+
+    float mean_white = (float) *sum_white / (float) *nb_white;
+    float mean_black = (float) *sum_black / (float) *nb_black;
+
+    float var_white = (float) *sum2_white / (float) *nb_white - (mean_white * mean_white);
+    float var_black = (float) *sum2_black / (float) *nb_black - (mean_black * mean_black);
+
+    float weight_whitep = (float) *nb_white / (float) (*nb_black + *nb_white);
+    float weight_blackp = 1.0 - weight_whitep;
+
+    if (weight_blackp != 0 && weight_whitep != 0)
+        *result = weight_whitep * var_white + weight_blackp * var_black;
 }
 
-float otsu_criteria(unsigned char *buffer, size_t rows, size_t cols, size_t pitch, int threshold, int thx, int thy) {
+float otsu_criteria2(unsigned char *buffer, size_t rows, size_t cols, size_t pitch, int threshold, int thx, int thy) {
     dim3 threads(thx, thy);
     dim3 blocks(std::ceil(float(cols) / float(threads.x)),
                 std::ceil(float(rows) / float(threads.y)));
     
     unsigned int *d_nb_white_pi = mallocCpy<unsigned int>(0, sizeof(unsigned int));
     unsigned int *d_nb_black_pi = mallocCpy<unsigned int>(0, sizeof(unsigned int));
-
-    nb_bw_pixels<<<blocks, threads>>>(buffer, rows, cols, pitch, d_nb_white_pi, d_nb_black_pi, threshold);
-    cudaCheckError();
-    cudaDeviceSynchronize();
-    
     unsigned int *d_sum_white = mallocCpy<unsigned int>(0, sizeof(unsigned int));
     unsigned int *d_sum_black = mallocCpy<unsigned int>(0, sizeof(unsigned int));
+    unsigned int *d_sum2_white = mallocCpy<unsigned int>(0, sizeof(unsigned int));
+    unsigned int *d_sum2_black = mallocCpy<unsigned int>(0, sizeof(unsigned int));
 
-    sum_black_white<<<blocks, threads>>>(buffer, rows, cols, pitch, threshold, d_sum_white, d_sum_black);
-    cudaCheckError();
-    cudaDeviceSynchronize();
+    float *d_result = mallocCpy<float>(inf, sizeof(float));
 
-    float *d_mean_white = malloc1Dcuda<float>(sizeof(float));
-    float *d_mean_black = malloc1Dcuda<float>(sizeof(float));
+    otsu<<<blocks, threads>>>(buffer, rows, cols, pitch, threshold,
+                              d_nb_white_pi, d_nb_black_pi,
+                              d_sum_white, d_sum_black,
+                              d_sum2_white, d_sum2_black);
+    /* cudaCheckError(); */
+    /* cudaDeviceSynchronize(); */
+
+    final_work<<<1, 1>>>(d_nb_white_pi, d_nb_black_pi,
+                         d_sum_white, d_sum_black,
+                         d_sum2_white, d_sum2_black,
+                         d_result);
     
-    mean<<<1, 1>>>(d_mean_white, d_sum_white, d_nb_white_pi);
-    mean<<<1, 1>>>(d_mean_black, d_sum_black, d_nb_black_pi);
-    cudaCheckError();
-    cudaDeviceSynchronize();
-    
-    float *d_var_white = mallocCpy<float>(0, sizeof(float));
-    float *d_var_black = mallocCpy<float>(0, sizeof(float));
+    /* cudaCheckError(); */
+    /* cudaDeviceSynchronize(); */
 
-    almost_var_black_white<<<blocks, threads>>>(buffer, rows, cols, pitch, threshold, d_mean_white, d_mean_black, d_var_white, d_var_black);
-    cudaCheckError();
-    cudaDeviceSynchronize();
-   
-    float h_var_white;
-    float h_var_black;
-    unsigned int h_nb_white_pi;
-    unsigned int h_nb_black_pi;
-    
-    cudaMemcpy(&h_var_white, d_var_white, sizeof(float), cudaMemcpyDeviceToHost);
-    cudaMemcpy(&h_var_black, d_var_black, sizeof(float), cudaMemcpyDeviceToHost);
-    cudaMemcpy(&h_nb_white_pi, d_nb_white_pi, sizeof(unsigned int), cudaMemcpyDeviceToHost);
-    cudaMemcpy(&h_nb_black_pi, d_nb_black_pi, sizeof(unsigned int), cudaMemcpyDeviceToHost);
 
-    h_var_white /= h_nb_white_pi;
-    h_var_black /= h_nb_black_pi;
+    float h_result;
+    cudaMemcpy(&h_result, d_result, sizeof(float), cudaMemcpyDeviceToHost);
 
-    float h_weight_whitep = (float) h_nb_white_pi / (float) (rows * cols);
-    float h_weight_blackp = 1.0 - (float) h_weight_whitep;
-
-    cudaFree(d_nb_white_pi);
-    cudaFree(d_nb_black_pi);
-    cudaFree(d_sum_white);
-    cudaFree(d_sum_black);
-    cudaFree(d_var_white);
-    cudaFree(d_var_black);
-
-    if (h_weight_whitep == 0 || h_weight_blackp == 0)
-        return inf;
-
-    return(float) h_weight_whitep * h_var_white + (float) h_weight_blackp * h_var_black;
+    return h_result;
 }
 
 unsigned char otsu_threshold(unsigned char* buffer, size_t rows, size_t cols, size_t pitch, int thx, int thy) {
     unsigned char opti_th = 0;
     float otsu_val = inf;
 
-    for (unsigned char i = 0; i < 255; i++) {
-        float otsu = otsu_criteria(buffer, rows, cols, pitch, i, thx, thy);
+    for (unsigned char i = 10; i < 103; i++) {
+        float otsu = otsu_criteria2(buffer, rows, cols, pitch, i, thx, thy);
         if (otsu < otsu_val) {
             otsu_val = otsu;
             opti_th = i;
         }
     }
+    
+    cudaCheckError();
+    cudaDeviceSynchronize();
 
     return opti_th;
 }
